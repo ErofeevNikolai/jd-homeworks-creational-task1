2007747760f289dee9357b95f51870cf16801948
ErofeevNikolai <erofeev_n@mail.ru>
2022-07-21 20:14:56 +0500

    initional commit

---
+++ src/main/java/ru/netology/PersonBuilder.java
@@ -0,0 +1,53 @@
+package ru.netology;
+
+import java.util.OptionalInt;
+
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private OptionalInt age = OptionalInt.empty();
+    private String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0 || age > 100) {
+            throw new IllegalArgumentException("Возраст недопустимый");
+        }
+        this.age = OptionalInt.of(age);
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalArgumentException("Не хватает обязательных полей");
+        }
+        if (this.age.isPresent()) {
+            Person person = new Person(name, surname, age);
+            if (address != null) {
+                person.setAddress(address);
+            }
+            return person;
+        } else {
+            Person person = new Person(name, surname);
+            if (address != null) {
+                person.setAddress(address);
+            }
+            return person;
+
+        }
+    }
+}
